7b65af5e8a71e018cb31ace037d2c2118bf7214d
S566938 <ramavathvinod5036@gmail.com>
2023-11-17 14:17:23 -0600

    update

--- controllers/horse.js
+++ controllers/horse.js
@@ -1,9 +1,9 @@
 var horse = require('../models/horse');
-// List of all horses
+// List of all horse
 exports.horse_list = async function(req, res) {
     try{
-    thehorses = await horse.find();
-    res.send(thehorses);
+    thehorse = await horse.find();
+    res.send(thehorse);
     }
     catch(err){
     res.status(500);
@@ -24,12 +24,13 @@ res.send('NOT IMPLEMENTED: horse delete DELETE ' + req.params.id);
 exports.horse_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: horse update PUT' + req.params.id);
 };
+
 // VIEWS
 // Handle a show all view
 exports.horse_view_all_Page = async function(req, res) {
     try{
-    thehorses = await horse.find();
-    res.render('horse', { title: 'horse Search Results', results: thehorses });
+    thehorse = await horse.find();
+    res.render('horse', { title: 'horse Search Results', results: thehorse });
     }
     catch(err){
     res.status(500);
@@ -67,4 +68,79 @@ exports.horse_detail = async function(req, res) {
     res.send(`{"error": document for id ${req.params.id} not found`);
     }
     };
+
+
+
+ // Handle horse delete on DELETE.
+exports.horse_delete = async function(req, res) {
+    console.log("delete " + req.params.id)
+    try {
+    result = await horse.findByIdAndDelete( req.params.id)
+    console.log("Removed " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": Error deleting ${err}}`);
+    }
+   };
+
+  
+   // Handle a show one view with id specified by query
+exports.horse_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await horse.findById( req.query.id)
+    res.render('horsedetail', { title: 'horse Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+
+   // Handle building the view for creating a horse.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.horse_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('horsecreate', { title: 'horse Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+
+
+   
+   // Handle building the view for updating a horse.
+// query provides the id
+exports.horse_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await horse.findById(req.query.id)
+    res.render('horseupdate', { title: 'horse Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   }
+
+   // Handle a delete one view with id from query
+exports.horse_delete_Page = async function(req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try{
+    result = await horse.findById(req.query.id)
+    res.render('horsedelete', { title: 'horse Delete', toShow: 
+   result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+   };
+   
     
--- routes/horse.js
+++ routes/horse.js
@@ -10,4 +10,18 @@ const horse_controlers= require('../controllers/horse');
 var router = express.Router();
 /* GET horses */
 router.get('/', horse_controlers.horse_view_all_Page );
+
+/* GET detail horse page */
+router.get('/detail', horse_controlers.horse_view_one_Page);
+
+/* GET create horse page */
+router.get('/create', horse_controlers.horse_create_Page);
+
+/* GET create update page */
+router.get('/update', horse_controlers.horse_update_Page);
+
+/* GET delete horse page */
+router.get('/delete', horse_controlers.horse_delete_Page)
+
+
 module.exports = router;
