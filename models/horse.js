9d828f7d0926897e51b48c7ba53253b93e151683
S566938 <ramavathvinod5036@gmail.com>
2023-11-29 17:50:34 -0600

    commit all

--- models/horse.js
+++ models/horse.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const horseSchema = mongoose.Schema({
 horse_color: String,
-horse_breed: String,
-horse_price: Number
+horse_breed: {type:String,length:15},
+horse_price: {type:Number,min:1000,max:100000}
 })
 module.exports = mongoose.model("horse",horseSchema)
--- routes/horse.js
+++ routes/horse.js
@@ -8,20 +8,29 @@ router.get('/', function(req, res, next) {
 var express = require('express');
 const horse_controlers= require('../controllers/horse');
 var router = express.Router();
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+  if (req.user){
+  return next();
+  }
+  res.redirect("/login");
+  }
 /* GET horses */
 router.get('/', horse_controlers.horse_view_all_Page );
 
 /* GET detail horse page */
-router.get('/detail', horse_controlers.horse_view_one_Page);
+router.get('/detail',secured, horse_controlers.horse_view_one_Page);
 
 /* GET create horse page */
-router.get('/create', horse_controlers.horse_create_Page);
+router.get('/create',secured, horse_controlers.horse_create_Page);
 
 /* GET create update page */
-router.get('/update', horse_controlers.horse_update_Page);
+router.get('/update',secured, horse_controlers.horse_update_Page);
 
 /* GET delete horse page */
-router.get('/delete', horse_controlers.horse_delete_Page)
+router.get('/delete',secured, horse_controlers.horse_delete_Page)
 
 
 module.exports = router;
